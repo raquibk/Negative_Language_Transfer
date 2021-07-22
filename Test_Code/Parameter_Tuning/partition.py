c815bd07a2a3b4e3a5eb5ee3d20075e8455c20fd
raquibk <r.raquib01@gmail.com>
2021-07-22 14:08:38 -0600

    Saving folds to txt instad of csv

--- Test_Code/Parameter_Tuning/partition.py
+++ Test_Code/Parameter_Tuning/partition.py
@@ -11,7 +11,7 @@ df_length = len(en_df.index)
 
 for i in range(1,6):
     upperbound = int(floor((df_length/5)*i))
-    en_df.iloc[lowerbound:upperbound].to_csv('../Resources/k-folds/partitioned_data/' + str(i)+new_en_df+'.csv')
-    zhs_df.iloc[lowerbound:upperbound].to_csv('../Resources/k-folds/partitioned_data/' + str(i)+new_zhs_df+'.csv')
+    en_df.iloc[lowerbound:upperbound].to_csv('../Resources/k-folds/partitioned_data/' + str(i)+new_en_df+'.txt')
+    zhs_df.iloc[lowerbound:upperbound].to_csv('../Resources/k-folds/partitioned_data/' + str(i)+new_zhs_df+'.txt')
     lowerbound = upperbound
 

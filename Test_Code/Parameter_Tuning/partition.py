9651cf0a9575cd1f2281f62c5b2db6cdf14a93a2
raquibk <r.raquib01@gmail.com>
2021-07-22 12:17:17 -0600

    Partitioned data

---
+++ Test_Code/Parameter_Tuning/partition.py
@@ -0,0 +1,17 @@
+from numpy import floor
+import pandas as pd
+
+en_df = pd.read_csv('../Resources/en_ud.txt')
+zhs_df = pd.read_csv('../Resources/zhs_ud.txt')
+new_en_df = '_en'
+new_zhs_df = '_zhs'
+upperbound = -999
+lowerbound = 0
+df_length = len(en_df.index)
+
+for i in range(1,6):
+    upperbound = int(floor((df_length/5)*i))
+    en_df.iloc[lowerbound:upperbound].to_csv('../Resources/k-folds/partitioned_data/' + str(i)+new_en_df+'.csv')
+    zhs_df.iloc[lowerbound:upperbound].to_csv('../Resources/k-folds/partitioned_data/' + str(i)+new_zhs_df+'.csv')
+    lowerbound = upperbound
+
